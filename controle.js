c660f5db2df34e2f6d3a75e3fb2215ef51bf06e1
A <A@MONSTER>
2025-01-25 10:22:45 -0300

    Adicionar arquivos de projeto.

---
+++ controle.js
@@ -0,0 +1,21 @@
+/**
+Comandos
+*/
+
+function on(){
+	document.getElementById("lamp").src = "on.jpg";
+}
+
+function off(){
+	document.getElementById("lamp").src = "off.jpg";
+}function blink(){
+	var intervalo = 0;
+	var contador = 0;
+	while(contador < 12){
+	intervalo += 250;
+	setTimeout("document.getElementById('lamp').src='on.jpg';",intervalo);
+	intervalo += 250;
+	setTimeout("document.getElementById('lamp').src='off.jpg';",intervalo);
+	contador++;
+	}
+}
